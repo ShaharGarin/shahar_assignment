facd0f983df5f808f3e94e1d75659bf2657651d4
ShaharGarin <121550005+ShaharGarin@users.noreply.github.com>
2024-04-29 17:19:50 +0300

    Update num_guess.py

--- day02/num_guess.py
+++ day02/num_guess.py
@@ -3,7 +3,12 @@ import random
 ran_num = random.randint(1,20)
 guess_count = 0
 while True:
-    user_num = round(float(((input("Guess a number between 1 and 20 (will round to closest int): ")))))
+    user_num = ((input("Guess a number between 1 and 20 (will round to closest int): ")))
+    if user_num.isnumeric() == False:
+        print("Only numbers are applicable. Try again.")
+        guess_count += 1
+        continue
+    user_num = int(float(user_num))
     if user_num < 1 or user_num > 20:
         print("Input wasn't a according to instructions. Try again!")
         guess_count += 1
