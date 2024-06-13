df67399865574a304c8ac05d342b1aab04b2499f
ShaharGarin <121550005+ShaharGarin@users.noreply.github.com>
2024-06-13 09:27:53 +0300

    Update fetch_proteins.py

--- day07/fetch_proteins.py
+++ day07/fetch_proteins.py
@@ -16,9 +16,9 @@ def main():
 
 def search_protein():
     if len(sys.argv) != 3 or not sys.argv[2].isnumeric():
-        raise Exception("Please provide search term and numberof files to download")
+        exit("Please provide search term and numberof files to download")
     if float(sys.argv[2]) > file_limit:
-        raise Exception("Too many files. NCBI limits download to 10,000")
+        exit("Too many files. NCBI limits download to 10,000")
     handle = Entrez.esearch(db = "protein", retmode = "txt", term = sys.argv[1], idtype = "acc", retmax = sys.argv[2])
     protein_search = Entrez.read(handle)
     handle.close()
@@ -28,7 +28,7 @@ def search_protein():
 def files_download(search_dic):
     for ide in search_dic["IdList"]:
         fetch = Entrez.efetch(db = "protein", id = ide, rettype = "fasta", retmode = "fasta")
-        file = f"{sys.argv[1]} sequence {ide}"
+        file = f"{sys.argv[1]}_sequence_{ide}.fna"
         with open(file, 'w') as pf:
             pf.write(fetch.read())
         print(file)
@@ -36,7 +36,7 @@ def files_download(search_dic):
     return
 
 def log_maker(search_dic):
-    log_name = "protein search log.csv"
+    log_name = "protein_search_log.csv"
     with open(log_name, 'a') as log:
         log.write(f"{dt.now().strftime("%m/%d/%Y %H:%M:%S"), sys.argv[1], sys.argv[2], search_dic["Count"]}\n")
     return
